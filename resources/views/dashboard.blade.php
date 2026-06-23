7ef9d0456bc99268036ef0769c9cead1bbfcb635
dalebulay8 <dalebulay8@gmail.com>
2026-06-23 10:57:37 +0800

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>PPATH Dashboard</title>
-        <link rel="icon" type="image/png" href="{{ asset('svg.png') }}">
+       <link rel="icon" href="/svg.png?v=3">
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"></script>
