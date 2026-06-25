2fd9226c4e74828f12ec05ec27248ce007c5f4d8
dalebulay8 <dalebulay8@gmail.com>
2026-06-25 15:12:28 +0800

    Update scanner.blade.php

--- resources/views/scanner.blade.php
+++ resources/views/scanner.blade.php
@@ -2,25 +2,59 @@
 <html>
 <head>
     <title>PPATH QR Scanner</title>
+
+    <script src="https://unpkg.com/html5-qrcode"></script>
+
 </head>
 <body>
 
-  <h1>PPATH QR Scanner</h1>
+    <h1>PPATH QR Scanner</h1>
 
-<h3>Select Attendance Table</h3>
+    <h3>Select Attendance Table</h3>
 
-<select>
+    <select id="activity_id">
 
-@foreach($activities as $activity)
+        @foreach($activities as $activity)
 
-<option value="{{ $activity->id }}">
+            <option value="{{ $activity->id }}">
+                {{ $activity->title }}
+            </option>
 
-{{ $activity->title }}
+        @endforeach
 
-</option>
+    </select>
 
-@endforeach
 
-</select>
+    <br><br>
+
+    <div id="reader"></div>
+
+
+    <script>
+
+        function onScanSuccess(decodedText) {
+
+            alert("Scanned: " + decodedText);
+
+        }
+
+
+        let html5QrcodeScanner = new Html5QrcodeScanner(
+
+            "reader",
+
+            {
+                fps: 10,
+                qrbox: 250
+            }
+
+        );
+
+
+        html5QrcodeScanner.render(onScanSuccess);
+
+
+    </script>
+
 </body>
 </html>
