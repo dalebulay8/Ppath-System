661f73360b4b973d753e83bb098e33bcd75febee
dalebulay8 <dalebulay8@gmail.com>
2026-07-09 10:23:29 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,23 +2,25 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MobileUploadController;
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| Here is where you can register API routes for your application.
+| These routes are loaded by the RouteServiceProvider within a group
+| assigned to the "api" middleware group.
 |
 */
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-use App\Http\Controllers\MobileUploadController;
 
 
 Route::post('/mobile-upload',
-[MobileUploadController::class,'upload']);
+    [MobileUploadController::class, 'upload']
+);
