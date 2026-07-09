7984f174c5c9676e1fb9e88b953bc7bd481a0876
dalebulay8 <dalebulay8@gmail.com>
2026-07-09 10:14:02 +0800

    Update MobileUpload.php

--- app/Models/MobileUpload.php
+++ app/Models/MobileUpload.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class MobileUpload extends Model
 {
+    protected $table = 'mobile_uploads';
+
     protected $fillable = [
         'table_name',
         'name',
